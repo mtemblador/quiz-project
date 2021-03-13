9265dea9d2b689fc709bd7e6a474c4ffc5d99847
Michael Temblador <michaeltemblador@Michaels-iMac.local>
2021-03-13 10:08:20 -0800

    button

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,30 +1,103 @@
-var startBtn = document.getElementById('startbutton');
-var timerEl = document.getElementById('timer');
+const startBtn = document.getElementById('startbutton');
+const timerEl = document.getElementById('timer');
+const directions = document.querySelector('.Directions');
+const answers = document.querySelector('.answers');
+const question = document.querySelector ('.Question');
+var questionIndex = 0 
+var choicesIndex = 0
+var score = 0
+var currentQuestion = [
+  {
+    question: "What is 2+2?",
+    choices: ['2','23','4','3'],
+    answer: '2', 
+  },
+  {
+    question: "How old am I?",
+    choices: ['3','36','35','56'],
+    answer: '35', 
+  }
+]
+//Empty divs//
+
+function getChoices(){
+  directions.innerHTML=""
+  question.innerHTML=""
+  answers.innerHTML=""
+ 
+  //Question//
+  var current = currentQuestion[questionIndex]
+  directions.textContent = current.question
+ 
+  //Answer//
+  var answerChoices = currentQuestion[choicesIndex]
+  question.textContent = answerChoices.choices
+  //For Each Loop
+  current.choices.forEach(function(item, index){
+ //Create new Button
+  var newButton = document.createElement("button");
+  newButton.setAttribute("type","button")
+  answerChoices.textContent = index + "" +item
+  newButton.textContent=index+1 + "- + item";
+  document.answerChoices.appendChild(newButton)
+}
+  )}
+//function answerChoiceA (answerTextInputA) { 
+   
+
+//function answerChoiceB (answerTextInputB) { 
+  //var newButton = document.createElement('BUTTON')
+ // newButton.innerHTML = answerTextInputB;
+ // document.body.appendChild(newButton); 
+//}
+//function answerChoiceC (answerTextInputC) { 
+ // var newButton = document.createElement('BUTTON')
+ // newButton.innerHTML = answerTextInputC;
+ // document.body.appendChild(newButton); 
+//}
+//function answerChoiceD (answerTextInputD) { 
+ // var newButton = document.createElement('BUTTON')
+ // newButton.innerHTML = answerTextInputD;
+  //document.body.appendChild(newButton); 
+//}
+
+function questionClick (){
+
+}
 
 function countdown() {
     var timeLeft = 30;
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function() {
-      // As long as the `timeLeft` is greater than 1
       if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
         timerEl.textContent = timeLeft;
-        // Decrement `timeLeft` by 1
         timeLeft--;
       } else if (timeLeft === 1) {
-        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
         timerEl.textContent = timeLeft;
         timeLeft--;
       } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
         timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
         clearInterval(timeInterval);
-        // Call the `displayMessage()` function
         displayMessage();
       }
-    }, 1000);
-  }
+   
+    }, 1000);}
+function runquiz () {
+  //startquiz ();
+  countdown ();
+  getChoices();
+  //var questionText = "What is your name?"
+  //addQuestion();
+  //answerChoiceA(curentQuestion[0].choice1);
+  //answerChoiceB(curentQuestion[0].choice2);
+  //answerChoiceC(curentQuestion[0].choice3);
+  //answerChoiceD(curentQuestion[0].choice4);
+  console.log ("test");
+}
+  startBtn.onclick = runquiz;
 
-  startBtn.onclick = countdown;
+ //To Do//
+ //Step one: Display questions and answers 
+ //Step two: Add Event Listener to each button to go to next question and track score
+ //Step three: Even Listener repeats step one with the next set up q/A
+ //Step three 1/2: Track score 
+ //Step four: Go to the score screen
