7f59311474d42698b21dbdb609bf4e0447d239aa
Michael Temblador <michaeltemblador@Michaels-iMac.local>
2021-03-14 21:01:08 -0700

    Score update

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,88 +2,111 @@ const startBtn = document.getElementById('startbutton');
 const timerEl = document.getElementById('timer');
 const directions = document.querySelector('.Directions');
 const answers = document.querySelector('.answers');
-const question = document.querySelector ('.Question');
+const question = document.querySelector('.Question');
 const highscore = document.querySelector(".Highscore");
-var questionIndex = 0 
+var questionIndex = 0
 var choicesIndex = 0
+var anwserIndex = 0
 var score = 0
-var currentQuestion = [
+var questionArray= [
   {
     question: "What is 2+2?",
-    choices: ['2','23','4','3'],
-    answer: '4', 
+    choices: ['2', '23', '4', '3'],
+    answer: '4',
   },
   {
     question: "How old am I?",
-    choices: ['3','36','35','56'],
-    answer: '35', 
+    choices: ['3', '36', '35', '56'],
+    answer: '35',
+  },
+  {
+    question: "How old is my daughter?",
+    choices: ['2', '1', '5', '3'],
+    answer: '2',
+  },
+  {
+    question: "What is my wife's name?",
+    choices: ['Priscyla', 'Rhea', 'Donna', 'Jane'],
+    answer: 'Priscyla',
+  },
+  {
+    question: "How long did this activity take me?",
+    choices: ['1 hour', '2 hours', '15 hours', 'FOR EVER!'],
+    answer: 'FOR EVER',
+  },
+  {
+    question: "Who is the current President",
+    choices: ['Donald Trump', 'George Washington', 'Joe Biden', 'Barack Obama'],
+    answer: 'Joe Biden',
   }
 ]
 //Empty divs//
+function setCounterText() {
+highscore.textContent = score
+}
+function getChoices() {
+  directions.innerHTML = ""
+  question.innerHTML = ""
+  answers.innerHTML = ""
+  highscore.innerHTML = "Your Score:" + score
 
-function getChoices(){
-  directions.innerHTML=""
-  question.innerHTML=""
-  answers.innerHTML=""
-  highscore.innerHTML= "Your Score:" + score
-  
 
   //Question//
-  var current = currentQuestion[questionIndex]
+  var current = questionArray[questionIndex]
   directions.textContent = current.question
-  //For Each Loop
-  current.choices.forEach(function(item, index){
-  var newButton = document.createElement("button");
-  newButton.className="multipleC";
-  newButton.setAttribute("type","button");
-  newButton.textContent= item,index;
-  question.appendChild(newButton);
-  console.log(item);
+  current.choices.forEach(function (item, index) {
+    var newButtonA = document.createElement("button");
+    newButtonA.setAttribute("type", "button");
+    newButtonA.textContent = item, index;
+    question.appendChild(newButtonA);
+    console.log(questionIndex);
   });
 }
-  document.querySelector("body").addEventListener("click",function(event) {
-//     //for (var i=0; i< currentQuestion.length; i ++){
-//     var currentAnswer = currentQuestion[i].choices;
-//     if (
-//       currentAnswer === currentQuestion[i].answer){
-//       score+=100;
-//       }
-//  // }
-//   //get attribute of a value on the button//
-    
-    
-      
-      console.log(currentAnswer)
-    //if (currentQuestion[0].answer
-    // If
-    // if(event.target.matches(".multipleC")){
-    //   score +=100
-    //   return score
-//}   
-//   //If users choice = the correct answer 
-
-// 
-  
+question.addEventListener("click", function (event) {
+  var myVal = event.target.innerHTML
+  var correct = questionArray[anwserIndex].answer
+  if (myVal == correct) {
+    score += 100;
+  }else {
+    score -= 100;
+  }
+  questionIndex++
+  anwserIndex++
+  getChoices();
+  setCounterText()
+  console.log(correct)
+  console.log(myVal)
 })
+// function endOfGame(){
+// directions.innerHTML = ""
+//   question.innerHTML = ""
+//   answers.innerHTML = ""
+//   highscore.innerHTML = ""
+// }
+
+
+
 function countdown() {
   var timeLeft = 30;
-  var timeInterval = setInterval(function() {
+  var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
-    timerEl.textContent = timeLeft;
-    timeLeft--;
-  } else if (timeLeft === 1) {
-    timerEl.textContent = timeLeft;
-    timeLeft--;
-  } else {
-    timerEl.textContent = '';
-    clearInterval(timeInterval);
-    displayMessage();
-  }
-}, 1000);}
-function runquiz () {
-         countdown ();
-         getChoices();
-console.log ("test");
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+function runquiz() {
+  countdown();
+  getChoices();
+  endOfGame
+  console.log("test");
 }
 startBtn.onclick = runquiz;
 
